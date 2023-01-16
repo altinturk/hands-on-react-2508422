0464813af6016c7c71d4e765698777e2008ef1ed
Alp Altinturk <alpin9@gmail.com>
2023-01-16 11:42:23 +0000

    gui crashed..

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,17 @@
+import React, {Component} from "react"
+
+/*class Welcome extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render () {
+    return <h1>Class return from Welcome {this.props.name}  </h1>  
+  }
+}*/
+
+const Welcome = (props) => {
+  return <h1>Const return: {props.name} </h1>
+}
 function App() {
   const name = 'StarGazers'
   return (
@@ -5,7 +19,8 @@ function App() {
       <article>
         <hgroup>
           <img src="images/group.svg" alt="StarGazers Group" />
-          <h1>Meet the <i style={{ color: "SteelBlue" }}>{name}</i></h1>
+          <Welcome name="Stars" />
+          <Welcome name="Aliens" />
           <p>Members of an <b>intergalactic alliance</b><br />
             paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
           <button className="outline" onClick={() => alert('Hi there')}>Click Me</button>
